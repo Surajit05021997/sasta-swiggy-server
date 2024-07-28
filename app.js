4fd903120a2869508e944d425dacafe6f99983ce
Surajit Maity <surajitmaity12345@gmail.com>
2024-07-28 20:04:30 +0530

    updated order route

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ const corsOptions = {
 }
 
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send("Sasta swiggy server running");
@@ -26,7 +27,7 @@ app.post('/order', async (req, res) => {
     const instance = new Razorpay({ key_id: process.env.RAZORPAY_KEY_ID, key_secret: process.env.RAZORPAY_KEY_SECRET })
 
     const order = await instance.orders.create({
-      // amount: req.body.amount * 100,
+      amount: req.body.amount,
       amount: 50000,
       currency: "INR",
       receipt: "receipt#1",
